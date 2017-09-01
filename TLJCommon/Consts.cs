84561ee363636e002c5c9b53852765cfbc05ea02
zhangfengqwer <984992820@qq.com>
2017-09-01 12:00:15 +0800

    comcon

---
+++ TLJCommon/Consts.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace TLJCommon
+{
+    public class Consts
+    {
+        public enum Platform
+        {
+            Platform_Official,
+        }
+
+        public enum Code
+        {
+            Code_OK = 1000,
+            Code_LoginFail,
+        }
+
+        public static string Tag_Login = "Login";
+        public static string Tag_QuickRegister = "QuickRegister";
+    }
+}
