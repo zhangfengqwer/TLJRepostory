ca12e338ba6ddfa13887dd848530b1163ba5e722
zhangfengqwer <984992820@qq.com>
2017-09-01 15:44:34 +0800

    新增一些登录参数

--- TLJCommon/Consts.cs
+++ TLJCommon/Consts.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-
-namespace TLJCommon
+﻿namespace TLJCommon
 {
     public class Consts
     {
@@ -20,5 +15,10 @@ namespace TLJCommon
 
         public static string Tag_Login = "Login";
         public static string Tag_QuickRegister = "QuickRegister";
+        public static readonly string ACCOUNT = "account";
+        public static readonly string PASSWORD = "password";
+        public static readonly string UID = "uid";
+        public static readonly string TAG = "tag";
+        public static readonly string CODENAME = "code";
     }
 }
